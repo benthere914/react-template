5846fdcea197edeabdc692235e85e11f4e8d2063
Benjamin Rose <benthere914@gmail.com>
2021-12-10 21:55:19 -0600

    initialized front end

---
+++ src/components/home/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+const Home = () => {
+    return (
+        <>
+            <div className='homePage'>
+                <h1>The site is loaded. You can now edit the home page or add other components</h1>
+            </div>
+        </>
+    )
+}
+
+export default Home
